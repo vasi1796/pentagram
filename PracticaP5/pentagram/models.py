29103e9bd09f9ff5ab51e2eb231ee972f11f8c7a
andrei.home <vasy1796@gmail.com>
2016-07-14 13:22:58 +0300

    first models done(photo and comment)

--- PracticaP5/pentagram/models.py
+++ PracticaP5/pentagram/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Photo(models.Model):
+    user = models.ForeignKey(User)
+    counter_like = models.IntegerField()
+    photo = models.ImageField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    photo_id = models.ForeignKey(Photo)
+    comment = models.CharField(max_length=50)
